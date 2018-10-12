7deab018d109cf67873b571faca2fc33934182bd
Marco Braun <17480141+Cherubeam@users.noreply.github.com>
2018-10-12 17:20:00 +0200

    Delete keys.js

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    tmdb: {
-        apiKey: '1cdcca0321bcfe6bdeba2672219b8d86'
-    },
-    trakt: {
-        clientId: '2b15aab339e4275722623a524e8ba70e2d841733a36937273c496b38240957bd',
-        clientSecret: 'df55dac6e4cbed60e0f2c6026930c9fbaee730a3cb99c0390e5d1053a68eed30'
-    }
-}
